8c6e90b27c8a9b7827d87e730396380061b5273d
Viran <verain.k1801@gmail.com>
2026-05-18 22:43:56 +0530

    fix some error in mail

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER", "smtp.gmail.com")
 app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT", 587))
 
 app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_DEFAULT_SENDER")
@@ -23,33 +24,28 @@ def home():
 
 @app.route("/contact", methods=["POST"])
 def contact():
+    try:
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
 
-    name = request.form.get("name")
-    email = request.form.get("email")
-    message = request.form.get("message")
+        msg = Message(
+            subject=f"New Portfolio Message from {name}",
+            recipients=["verain.k1801@gmail.com"]
+        )
 
-    # Create Email
-    msg = Message(
-        subject=f"New Portfolio Message from {name}",
-        recipients=["verain.k1801@gmail.com"]
-    )
-
-    msg.body = f"""
-You received a new portfolio message.
-
+        msg.body = f"""
 Name: {name}
-
 Email: {email}
-
-Message:
-{message}
+Message: {message}
 """
 
-    # Send Mail
-    mail.send(msg)
-    
+        mail.send(msg)
 
-    return render_template("index.html", success=True)
+        return render_template("index.html", success=True)
+
+    except Exception as e:
+        return f"MAIL ERROR: {str(e)}"
 
 if __name__ == "__main__":
     app.run(debug=True)
